600f23d3a67a82c1e1d3607b8aea542f911b46c2
azabraao <az.abraao@gmail.com>
2020-04-28 19:39:21 -0300

    Add vagas migration

--- database/migrations/2020_04_28_222107_create_vagas_table.php
+++ database/migrations/2020_04_28_222107_create_vagas_table.php
@@ -14,8 +14,13 @@ class CreateVagasTable extends Migration
     public function up()
     {
         Schema::create('vagas', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->bigIncrements('id');
+            $table->integer('numero');
+            $table->string('descricao');
+            $table->string('setor_id');
+            $table->foreign('setor_id')
+            ->references('id')
+            ->on('setores');
         });
     }
 
